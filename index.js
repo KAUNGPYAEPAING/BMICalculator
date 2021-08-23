4e2521181ec1f53f9f237e2c03ab2526a7a02223
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-08-23 15:08:26 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+var weight = prompt("What is your weight?");
+var height = prompt("What is your height?")
 
 function bmiCalculator(weight, height){
     var bmi = weight / Math.pow(height,2);
     return Math.round(bmi);
 }
 
-bmiCalculator(65, 1.8);
+var BMI = bmiCalculator(weight, height);
+
+if(BMI < 18.5){
+    alert("Your BMI is " + BMI +", so you are underweight");
+}
+
+if(BMI >= 18.5 && BMI<=24.9){
+    alert("Your BMI is " + BMI + ",so you have a normal weight");
+}
+
+if(BMI > 24.9){
+    alert("Your BMI is "+ BMI +",so you are overweight");
+}
+
